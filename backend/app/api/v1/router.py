34268dcea1f99a2c8f61e9171a709dbdc7748439
Sofia1653 <sofedu2005@gmail.com>
2026-09-25 17:48:39 -0300

    add: people and reviews

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
 from app.movies.router import router as movies_router
+from app.reviews.router import router as reviews_router
+from app.people.router import router as people_router
 
 api_router = APIRouter()
 
-api_router.include_router(movies_router) 
+api_router.include_router(movies_router)
+api_router.include_router(reviews_router)
+api_router.include_router(people_router)
